dcdc407da534e515291f8ea01201f9e930cb83de
sheraz-ali <soomrosheraz054@gmail.com>
2026-08-16 22:41:28 +0500

    Air pods app development
    Profile page UI development

--- lib/constants/string_const.dart
+++ lib/constants/string_const.dart
@@ -6,4 +6,7 @@ class StringConst {
   The technology with two noise sensors and two microphones on each ear cup detects ambient noise and sends the data to the HD noise minimization processor QN1. Using a new algorithm, the QN1 then processes and minimizes noise for different acoustic environments in real time. Together with a new Bluetooth Audio SoC
   ''';
 
+  static String get profilePictureUrl => 'https://media.licdn.com/dms/image/v2/D4D03AQF10MKF7Y4G2w/profile-displayphoto-shrink_400_400/B4DZO_EsajGUAg-/0/1734077505137?e=1788393600&v=beta&t=4KnubrYwu5fgtqlaK_ck89pMOcjKytrNlDYIDGnxuhI';
+
+
 }
--- lib/presentation/screens/profile_screen.dart
+++ lib/presentation/screens/profile_screen.dart
@@ -1,11 +1,58 @@
+import 'package:airpods_app/constants/string_const.dart';
+import 'package:airpods_app/core/app_colors.dart';
 import 'package:airpods_app/core/app_textstyles.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget{
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Profile screen", style: AppTextStyles.headingTextStyle,),);
+    return Padding(padding: .symmetric(horizontal: 16, vertical: 24), child: Column(
+      spacing: 24,
+      crossAxisAlignment: .start,
+      children: [
+        Align(
+          alignment: .center,
+          child: Column(
+            spacing: 16,
+            children: [
+              Text('About Me', style: AppTextStyles.subHeadingTextStyle,),
+              ClipOval(
+                child: Image.network(StringConst.profilePictureUrl, height: 100, width: 100),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          spacing: 8,
+          crossAxisAlignment: .start,
+          children: [
+            Text("Achievements", style: AppTextStyles.subHeadingTextStyle,),
+            Column(
+              children: [
+                _buildBulletPointAchievementItemWidget('Completed and Published 15+ Apps on the App Store and Play Store'),
+                _buildBulletPointAchievementItemWidget('300+ hours on Upwork'),
+                _buildBulletPointAchievementItemWidget('Top rated Freelancer on Upwork'),
+
+              ],
+            )
+          ],
+        )
+      ],
+    ),);
+  }
+
+  Widget _buildBulletPointAchievementItemWidget(String achievement) {
+    return Row(
+      spacing: 10,
+      children: [
+        CircleAvatar(
+          backgroundColor: AppColors.greyColor,
+          radius: 4,
+        ),
+        Expanded(child: Text(achievement, style: AppTextStyles.regularTextStyle,))
+      ],
+    );
   }
 }
